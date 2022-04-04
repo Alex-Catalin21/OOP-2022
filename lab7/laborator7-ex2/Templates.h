22571e57975c47ddfbddb6e2158e168b2b1709d9
Alex-Catalin21 <alex_catalin_2009@yahoo.com>
2022-04-04 19:58:54 +0300

    update

---
+++ lab7/laborator7-ex2/Templates.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template<class T>
+class MyTemplate {
+	T *arr = new T(2);
+public:
+	void push(T val) {
+
+	}
+	T pop() {
+
+	}
+	void remove(int index) {
+
+	}
+	void insert(T elem, int index) {
+
+	}
+};
